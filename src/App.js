80b825686ce455ae3634579bc203ad9c9bd89220
LanceLancealot <maxfield.kraus@gmail.com>
2024-01-23 16:43:24 -0500

    added some structure file content

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import About from '../components/About';
+import Portfolio from './components/Portfolio';
+import Contact from '../components/Contact';
+import Resume from './components/Resume';
+import {Helmet} from 'react-helmet';
+import './App.css';
+
+function App() {
+    const [currentTab, handleTabChange] = useState('about');
+
+    const renderTab = () => {
+        if (currentTab === 'about') {
+            return <About />;
+        }
+        if (currentTab === 'Contact') {
+            return <Contact />;
+        }
+        if (currentTab === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentTab === 'Resume') {
+            return <Resume />;
+        }
+        return <About />;
+    };
+    return (
+        <>
+            <Helmet>
+                <title>Maxfield Kraus' Portfolio | {currentTab} </title>
+            </Helmet>;
+            <Header 
+                currentTab={currentTab} handleTabChange={handleTabChange}>  
+            </Header>
+            <main>
+                {renderTab()}
+            </main>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default App;
